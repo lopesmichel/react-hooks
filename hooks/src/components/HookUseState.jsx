2771109ebadd4d3880ea104e5becccd2b506c8a9
Michel Lopes <lopesmichel.lp@gmail.com>
2025-05-13 21:13:18 -0300

    testando usestate

---
+++ hooks/src/components/HookUseState.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {useState} from "react";
+
+const HookUseState = () => {
+    //-1 useState
+    let userName = "João"
+
+    const [name, setName] = useState("Matheus")
+
+    const changeNames = () => {
+
+        userName = "João souza"
+
+        setName ("Michel Lopes")
+    }
+  return (
+    <div>
+      {/* 1 - useState*/}
+      <h2>useState</h2>
+      <p>Variável: {userName}</p>
+      <p>useState: {name}</p>
+      <button onClick={changeNames}>Mudar nomes</button>
+      <hr />
+    </div>
+  );
+  
+}
+
+export default HookUseState
