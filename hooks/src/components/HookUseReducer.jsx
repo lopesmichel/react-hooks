1e48ef598948e0e1595a4976b1534b4c47371347
Michel Lopes <lopesmichel.lp@gmail.com>
2025-05-18 18:26:43 -0300

    hook reducer

---
+++ hooks/src/components/HookUseReducer.jsx
@@ -0,0 +1,18 @@
+import React, { useReducer } from 'react'
+
+const HookUseReducer = () => {
+
+    const [number, dispatch] = useReducer((state, action) => {
+        return Math.random(state);
+    })
+  return (
+    <div>
+        <h2>useReducer</h2>
+        <p>Número: {number}</p>
+        <button onClick={dispatch}>Alterar número</button>
+        <hr />
+    </div>
+  )
+}
+
+export default HookUseReducer
