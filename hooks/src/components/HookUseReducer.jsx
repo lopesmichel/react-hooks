3c9f6f05c895702cccbd6d1e0933765e4ed5521c
Michel Lopes <lopesmichel.lp@gmail.com>
2025-05-19 22:08:28 -0300

    reducer add e delete

--- hooks/src/components/HookUseReducer.jsx
+++ hooks/src/components/HookUseReducer.jsx
@@ -1,18 +1,67 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from "react";
 
 const HookUseReducer = () => {
+  const [number, dispatch] = useReducer((state, action) => {
+    return Math.random(state);
+  });
 
-    const [number, dispatch] = useReducer((state, action) => {
-        return Math.random(state);
-    })
+  const initialTasks = [
+    { id: 1, text: "Fazer alguma coisa" },
+    { id: 2, text: "Fazer outra coisa" },
+  ];
+
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        const newTask = {
+          id: Math.random(), // corrigido aqui
+          text: taskText,
+        };
+
+        setTaskText("");
+
+        return [...state, newTask];
+      case "DELETE":
+        return state.filter((task) => task.id !== action.id);
+      default:
+        return state;
+    }
+  };
+
+  const [taskText, setTaskText] = useState("");
+  const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatchTasks({ type: "ADD" });
+  };
+
+  const removeTask = (id) => {
+    dispatchTasks({ type: "DELETE", id });
+  };
+
   return (
     <div>
-        <h2>useReducer</h2>
-        <p>Número: {number}</p>
-        <button onClick={dispatch}>Alterar número</button>
-        <hr />
+      <h2>useReducer</h2>
+      <p>Número: {number}</p>
+      <button onClick={dispatch}>Alterar número</button>
+      <h3>Tarefas:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTaskText(e.target.value)}
+          value={taskText}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      {tasks.map((task) => (
+        <li key={task.id} onDoubleClick={() => removeTask(task.id)}>
+          {task.text}
+        </li>
+      ))}
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default HookUseReducer
+export default HookUseReducer;
